28a73c3362f1c9c99a022c37d20f34a75a37afe3
Kobzarko <evgewi4@gmail.com>
2020-01-11 15:06:37 +0200

    Added functions start and chooseExpenses and checkSavings

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
-let money = +prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+'use strict';
 
+// объявляем глобальные переменные
+let money, time;
+
+function Start() {
+	// ставим пустые кавычки для записи результата в переменную 
+	money = +prompt("Ваш бюджет на месяц?", ''),
+		time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+	while (isNaN(money) || money == "" || money == null) {
+		money = +prompt("Ваш бюджет на месяц?", '');
+	}
+}
+
+Start();
+
+// создаем объект 
 let appData = {
 	budget: money,
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
-	savings: false
+	savings: true
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+function chooseExpenses() {
 
-// appData.expenses.a1 = a2;
-// appData.expenses.a3 = a4;
+	for (let i = 0; i < 2; i++) {
+		let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+			b = prompt("Во сколько обойдется?", '');
+		if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+			console.log("well done");
+			appData.expenses[a] = b;
+		} else {
+			// с помощью декремента возвращаем цикл на единицу обратно
 
-for(let i = 0 ; i<2; i++)
-{
-	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-		b = prompt("Во сколько обойдется?", '');
-	if ( (typeof(a))==='string' && (typeof(a))!=null && (typeof(b))!=null && a != '' && b != '' && a.length < 50 ) {
-		console.log("well done");
-		appData.expenses[a] = b;
+			i--;
+		}
+
 	}
-	else {
-		alert("Введите правильные данные");
-		i--;
-	}
-	
 }
+
+chooseExpenses();
+
 
 // loop while
 // let i=0;
@@ -70,20 +82,32 @@ for(let i = 0 ; i<2; i++)
 
 
 
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
-alert("Ежедневный бюджет составляет : "  + appData.moneyPerDay + " UAH.");
+alert("Ежедневный бюджет составляет : " + appData.moneyPerDay + " UAH.");
 
-if (appData.moneyPerDay< 100) {
+if (appData.moneyPerDay < 100) {
 	console.log("Минимальный уровень достатка ")
-}else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
 	console.log("Средний уровень достатка")
-}else if ( appData.moneyPerDay > 2000) {
+} else if (appData.moneyPerDay > 2000) {
 	console.log("Средний уровень достатка")
-}else {
+} else {
 	console.log("new Error(message: string)")
 }
 
 
 
+function checkSavings() {
+	if (appData.savings == true) {
+		let save = +prompt("Какая сумма накоплений?"),
+			percent = +prompt("Под какой процент?");
 
+		appData.monthIncome = save / 100 / 12 * percent;
+		alert("Доход с вашего депозита в месяц " + appData.monthIncome);
+	}
+}
+
+checkSavings();
+
+// Функция для определения необязательных расходов
